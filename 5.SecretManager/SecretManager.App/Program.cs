64bf6373a7348250ed2bb8f7cfa09df2e8d77d58
Nuttakorn Tedthong <leendsru@hotmail.com>
2023-03-16 18:10:28 +0700

    SecretManagers

---
+++ 5.SecretManager/SecretManager.App/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+
+var secretManagerClient = new AmazonSecretsManagerClient();
+
+var listSecretVersionRequest = new ListSecretVersionIdsRequest
+{
+    SecretId = "ApiKey",
+    IncludeDeprecated = true,
+};
+
+var versionResponse = await secretManagerClient.ListSecretVersionIdsAsync(listSecretVersionRequest);
+
+var request = new GetSecretValueRequest
+{
+    SecretId = "ApiKey",
+    //VersionId = versionResponse.Versions[0].VersionId,
+    //VersionStage = "AWSCURRENT"
+};
+
+var response = await secretManagerClient.GetSecretValueAsync(request);
+
+Console.WriteLine(response.SecretString);
